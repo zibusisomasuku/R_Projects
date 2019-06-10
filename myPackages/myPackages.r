7553758d56b21e6f0f0155727d6b2f4c2183ff19
Zibusiso Masuku <45422945+zibusisomasuku@users.noreply.github.com>
2019-06-10 18:53:09 +0200

    Create myPackages.r
    List of packages I use

---
+++ myPackages/myPackages.r
@@ -0,0 +1,7 @@
+my_packages <- c("tidyverse", "broom", "coefplot", "cowplot",
+	"gapminder", "GGally", "ggrepel", "ggridges", "gridExtra",
+	"here", "interplot", "margins", "maps", "mapproj",
+	"mapdata", "MASS", "quantreg", "rlang", "scales",
+	"survey", "srvyr", "viridis", "viridisLite", "devtools","readxl","readr","dplyr","plyr","tictoc")
+
+install.packages(my_packages)
