14068296407aa1c91eb9f37b92de5541180adfbd
Zibusiso Masuku <45422945+zibusisomasuku@users.noreply.github.com>
2019-06-25 10:04:56 +0200

    Update data_eda.r

--- hc_proj/data_eda.r
+++ hc_proj/data_eda.r
@@ -1,6 +1,6 @@
 #Clearing the workspace
 rm (list = ls())
-claims_data = readRDS("./output/2013-18 data.rds")
+claims_data = readRDS("./output/2013-18 final data.rds")
 
 #Load Libraries
 library(readxl)
@@ -8,8 +8,14 @@ library(data.table)
 library(dplyr)
 library(tidyr)
 library(inspectdf)
+library(DataExplorer)
 
 #Set Working Directory on R Studio
 #You may need to consider other methods like the here() library for other IDEs
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+plot_str(claims_data)
+plot_missing(claims_data)
+plot_histogram(claims_data)
+plot_density(claims_data)
+plot_bar(claims_data)
